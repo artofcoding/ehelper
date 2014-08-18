aef8f0347a51ba4df3c2eb99326381747e22f8e8
Ralf Bensmann <ralf@art-of-coding.eu>
2014-08-18 20:54:26 +0200

    @Ignored test

--- src/test/java/eu/artofcoding/emf/XsdToEcoreTest.java
+++ src/test/java/eu/artofcoding/emf/XsdToEcoreTest.java
@@ -1,15 +1,19 @@
-package eu.artofcoding.test.emf;
+package eu.artofcoding.emf;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class XsdToEcoreTest {
 
     @Test
-    public void xsdToEcore() {
-        Path xsd = Paths.get("src/main/resources/request.xsd");
+    @Ignore
+    public void xsdToEcore() throws Exception {
+        URI uri = XsdToEcoreTest.class.getResource("request.xsd").toURI();
+        Path xsd = Paths.get(uri);
         Path ecore = Paths.get("request.ecore");
         EModelHelper emh = EModelHelper.eINSTANCE();
         emh.xsdToEcore(xsd, ecore);
